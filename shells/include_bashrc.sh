792c5f4ef2250aaae8c81d32bdf73bac8509cf48
Ryota Funaki <contact@ngv.jp>
2024-07-11 12:03:55 +0900

    Fix Kiota (#30)

---
+++ shells/include_bashrc.sh
@@ -0,0 +1,4 @@
+
+if [ -d "$HOME/.dotnet/tools" ]; then
+    export PATH="$PATH:$HOME/.dotnet/tools"
+fi
---
+++ shells/install_kiota.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# We'll only build amd64 until we can use Docker Hub Automated Builds to build containers using other architectures.
+ARCH=$(dpkg --print-architecture)
+
+if [[ "$ARCH" != "amd64" ]]; then
+    echo "workload is not available"
+    exit 0
+fi
+
+# Install development tools
+sudo dotnet workload update
+dotnet tool install --global Microsoft.OpenApi.Kiota
